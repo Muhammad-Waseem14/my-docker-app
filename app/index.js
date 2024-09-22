e10e21ac9dee7f2930bb277d7641a9a38d44ec0b
Muhammad-Waseem14 <muhammad.wasim@arbisoft.com>
2024-09-22 20:28:56 +0500

    Docker compose file

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from docker" });
+  res.json({ message: "Hello from docker...!" });
 });
 
 app.listen(9000, () => console.log("Server is running!"));
